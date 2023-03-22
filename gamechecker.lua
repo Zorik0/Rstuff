2e1aa89cb990e5e6c51581015083799ad8eb7dfc
Zorik0 <128587632+Zorik0@users.noreply.github.com>
2023-03-22 20:52:45 +0530

    Update gamechecker.lua

--- gamechecker.lua
+++ gamechecker.lua
@@ -2,6 +2,7 @@ local placeId = game.PlaceId
 --[[FART RACE]]
 if placeId == 11763462079 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/one.lua"))()
+--[[Mega Mansion Tycoon]]
 elseif placeId == 8328351891 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/two/two.lua"))()
   end
