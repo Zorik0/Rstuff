aa761a54d059ded440e2c7108e719e2f49034823
Zorik0 <128587632+Zorik0@users.noreply.github.com>
2023-03-22 16:38:56 +0530

    Update gamechecker.lua

--- gamechecker.lua
+++ gamechecker.lua
@@ -1,7 +1,7 @@
 local placeId = game.PlaceId
 --[[FART RACE]]
 if placeId == 11763462079 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/one.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/one.lua"))()
 elseif placeId == 8328351891 then
-    print("executed")
+print("executed")
 
