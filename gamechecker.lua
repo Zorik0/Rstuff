638873b6007b34da955c9ad760d68392413505cc
Zorik0 <128587632+Zorik0@users.noreply.github.com>
2023-03-22 16:29:51 +0530

    Update gamechecker.lua

--- gamechecker.lua
+++ gamechecker.lua
@@ -4,4 +4,4 @@ if placeId == 11763462079 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/one.lua"))()
 elseif placeId == 8328351891 then
     print("executed")
-end
+
