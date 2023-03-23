bbcc3f8b65b18835cad1acdcddbe4678835876a4
Zorik0 <128587632+Zorik0@users.noreply.github.com>
2023-03-23 23:38:56 +0530

    Update gamechecker.lua

--- gamechecker.lua
+++ gamechecker.lua
@@ -7,8 +7,9 @@ elseif placeId == 8328351891 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/two/two.lua"))()
 elseif placeId == 11124905486 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLking123456/Map/main/Shoot"))()
+elseif placeId == 4566572536 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/three.lua"))()
   end
-
 
 --[[Send Embed]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/s_execute.lua"))()
