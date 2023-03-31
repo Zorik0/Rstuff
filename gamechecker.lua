c0c03c01373382a3dfba973ca25b6d111dd482d5
Zorik0 <128587632+Zorik0@users.noreply.github.com>
2023-03-31 09:55:42 +0530

    Update gamechecker.lua

--- gamechecker.lua
+++ gamechecker.lua
@@ -5,8 +5,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/on
 --[[Mega Mansion Tycoon]]
 elseif placeId == 8328351891 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/two/two.lua"))()
-elseif placeId == 11124905486 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLking123456/Map/main/Shoot"))()
 elseif placeId == 4566572536 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/Rstuff/main/vl.lua"))()
 elseif placeId == 574407221 then
