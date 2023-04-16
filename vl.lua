68afb1634e80f60e8b455646ffcf673791c54455
Zorik0 <128587632+Zorik0@users.noreply.github.com>
2023-04-17 03:28:22 +0530

    Update vl.lua

--- vl.lua
+++ vl.lua
@@ -1,30 +1,8 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/dirt",true))()
 local Table = {}
 local window = Lib:CreateWindow("Vehicle Legends")
-window:Section("Auto Farm Eggs")
+window:Section("Gameplay")
 
--- get the eggs folder in Workspace
-local eggsFolder = game:GetService("Workspace").Eggs
-
--- define the toggle function
-window:Toggle("Egg Farm", {location = Table, flag = "Toggle"}, function()
-    -- loop through all the child objects in the folder
-    for _, egg in pairs(eggsFolder:GetChildren()) do
-        -- get the local player's character's HumanoidRootPart
-        local character = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
-        
-        -- set the player's position to the egg's position
-        character.CFrame = egg.CFrame
-        
-        -- wait for 1 second
-        wait(1)
-        
-        -- print the name of the egg
-        print(egg.Name)
-    end
-    -- print the toggle status
-    print(Table["Toggle"])
-end)
 
 window:Button("Vehicle Fly [X]",function()
     local FlyKey = Enum.KeyCode.X
